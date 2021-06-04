d7278a9bd67f7e03f3a58f6f56110cd1ddd03372
Berend van Lochem <bvl46z@gmail.com>
2021-06-04 16:28:01 +0200

    Add project files.

---
+++ snake/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace snake
+{
+    partial class frmSnake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSnake));
+            this.picBoard = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoard
+            // 
+            this.picBoard.Location = new System.Drawing.Point(12, 12);
+            this.picBoard.Name = "picBoard";
+            this.picBoard.Size = new System.Drawing.Size(420, 420);
+            this.picBoard.TabIndex = 0;
+            this.picBoard.TabStop = false;
+            this.picBoard.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // imgList
+            // 
+            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgList.Images.SetKeyName(0, "bonus.png");
+            this.imgList.Images.SetKeyName(1, "body.png");
+            this.imgList.Images.SetKeyName(2, "head.png");
+            this.imgList.Images.SetKeyName(3, "brick.png");
+            // 
+            // frmSnake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 454);
+            this.Controls.Add(this.picBoard);
+            this.Name = "frmSnake";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmSnake_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoard;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imgList;
+    }
+}
+
